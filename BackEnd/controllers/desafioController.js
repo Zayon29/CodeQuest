5864ff787fc40e6ddc226de898769944d55f5418
Vitor Emanuel <vitor.emanueldsvidal@gmail.com>
2025-06-27 14:31:49 -0300

    feat: capture a random challenge

--- BackEnd/controllers/desafioController.js
+++ BackEnd/controllers/desafioController.js
@@ -29,18 +29,20 @@ const listarDesafios = async (req, res) => {
   }
 };
 
-//função apra retornar o desafio atual/diario a logica disso aqui pode mudar por enquanto esta como o ULTIMO CADASTRADO
 const desafioAtual = async (req, res) => {
   try {
-    const desafio = await Desafio.findOne().sort({ createdAt: -1 }); // ou por data: { data: -1 }
-    if (!desafio) {
+    const desafios = await Desafio.aggregate([{ $sample: { size: 1 } }]);
+
+    if (!desafios || desafios.length === 0) {
       return res.status(404).json({ message: 'Nenhum desafio encontrado.' });
     }
-    res.status(200).json(desafio);
+
+    res.status(200).json(desafios[0]);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
 
 const atualizarDesafio = async (req, res) => {
   const { id } = req.params;
